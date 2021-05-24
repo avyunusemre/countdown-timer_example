69f286129ec1175592817868c01b13929df0adc5
avyunusemre <deu.av.yunusemre@gmail.com>
2021-05-24 18:48:40 +0200

    countdown-timer

---
+++ app.js
@@ -0,0 +1,31 @@
+const daysText = document.querySelector("#days");
+const hoursText = document.querySelector("#hours");
+const minsText = document.querySelector("#mins");
+const secondsText = document.querySelector("#seconds");
+
+const newYears = "1 Jan 2022";
+
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = formatTime(Math.floor(totalSeconds / 3600 / 24));
+  const hours = formatTime(Math.floor(totalSeconds / 3600) % 24);
+  const minutes = formatTime(Math.floor(totalSeconds / 60) % 60);
+  const seconds = formatTime(Math.floor(totalSeconds % 60));
+
+  daysText.innerHTML = days;
+  hoursText.innerHTML = hours;
+  minsText.innerHTML = minutes;
+  secondsText.innerHTML = seconds;
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
